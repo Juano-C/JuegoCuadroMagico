867eb7a674550deaf6786db2bca5b2ee87880959
David <daviddanielc25@gmail.com>
2023-03-18 00:38:51 -0300

    Clases y Metodos implementados, revisar si hay que agregar/quitar algo, o repensar alguna cosa.

--- Ezequiel/JuegoCuadroMagico/src/logica/GrillaJuego.java
+++ Ezequiel/JuegoCuadroMagico/src/logica/GrillaJuego.java
@@ -1,37 +1,222 @@
 package logica;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class GrillaJuego {
 
 	private int[][] _grilla;
+	private Map<String, Integer> _resultados;
+
 	private int[][] _grillaSol;
+	private Map<String, Integer> _resultadosSolucion;
+	private int _tamanio;
 	
 	
+	/*
+	 * Genera la grilla tamanio x tamanio = n x n (cuadrada)
+	 */
 	public GrillaJuego(int tamanio) {
+		if(tamanio <= 0) {
+			throw new IllegalArgumentException("El tamanio debe ser un entero positivo: " + tamanio);
+		}
+		_tamanio = tamanio;
+		_resultados = new HashMap<String, Integer>();
+		_resultadosSolucion = new HashMap<String, Integer>();
+		_grilla = generarGrilla(tamanio);
+		_grillaSol = generarGrillaSolucion(tamanio);
+
+	
+	}
+
+	public void agregarNumero(int i, int j, int num) {
+		if(i < 0 || i >= _tamanio) {
+			throw new IllegalArgumentException("Fuera de rango: " + i);
+		}
+		if(j < 0 || j >= _tamanio) {
+			throw new IllegalArgumentException("Fuera de rango: " + j);
+		}
+		if(num <= 0) {
+			throw new IllegalArgumentException("El numero debe ser un entero positivo: " + num); 
+		}
 		
+		_grilla[i][j] = num;
+		_resultados.put( "f" + i   , _resultados.get("f" + i)  +  num );
+		_resultados.put( "c" + j   , _resultados.get("c" + j)  +  num );
 	}
 	
-	public void agregarNumero(int i, int j, int num) {}
+	
 	
 	// Podria llamarse borrarNumero - eliminarPoscion 
 	// sacar se podria referir a obtener 
-	public void sacarNumero(int i, int j) {}
 	
-	public void modificar(int i, int j, int num) {}
-	
-	private int obtenerResultado(String fila_o_col, int i) {
-		return 0;
+	public void sacarNumero(int i, int j) {
+		if(i < 0 || i >= _tamanio) {
+			throw new IllegalArgumentException("Fuera de rango: " + i);
+		}
+		if(j < 0 || j >= _tamanio) {
+			throw new IllegalArgumentException("Fuera de rango: " + j);
+		}
+		
+		_resultados.put( "f" + i   , _resultados.get("f" + i)  -  _grilla[i][j] );
+		_resultados.put( "f" + j   , _resultados.get("f" + j)  -  _grilla[i][j] );
+		_grilla[i][j] = 0;
+		
 	}
 	
+	public void modificar(int i, int j, int num) {
+		if(i < 0 || i >= _tamanio) {
+			throw new IllegalArgumentException("Fuera de rango: " + i);
+		}
+		if(j < 0 || j >= _tamanio) {
+			throw new IllegalArgumentException("Fuera de rango: " + j);
+		}
+		if(num <= 0) {
+			throw new IllegalArgumentException("El numero debe ser un entero positivo: " + num); 
+		}
+		
+		sacarNumero(i, j);
+		agregarNumero(i, j, num);
+	}
+	
+	// Falta implementar
 	private boolean estaCompletaFyC() {
 		return false;
 	}
 	
-	public void vaciarMatriz() {}
+	public void vaciarMatriz() {
+		_grilla = new int[_tamanio][_tamanio];
+		for(String f_c : _resultados.keySet()) {
+			_resultados.put(f_c, 0);
+		}
+	}
 	
 	public boolean estaCompletaMatriz() {
-		return false;
+		for(int i = 0; i < _tamanio; i++) {
+			// Comprobamos la suma de las filas
+			if(_resultados.get("f" + i) == _resultadosSolucion.get("f" + i)) {
+				return false;
+			}
+			// Comprobamos la suma de las columnas
+			if(_resultados.get("c" + i) == _resultadosSolucion.get("c" + i)) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	
+	public void imprimir() {
+		
+		for(int i = 0; i < _tamanio; i++) {
+			for(int j = 0; j < _tamanio; j++) {
+				if(_grilla[i][j] > 0) {
+					if(_grilla[i][j] > 9) {
+						System.out.print(" " +  _grilla[i][j]);
+					}else {
+						System.out.print("  " +  _grilla[i][j]);
+					}
+				}else {
+					System.out.print("  " + " ");
+				}
+			}
+			if(_resultadosSolucion.get("f" + i) > 9) {
+				System.out.println(" " +  _resultadosSolucion.get("f" + i));
+			}else {
+				System.out.println("  " +  _resultadosSolucion.get("f" + i));
+			}
+		}
+		
+		for(int j = 0; j < _tamanio ; j++) {
+			if(_resultadosSolucion.get("c" + j) > 9) {
+				System.out.print(" " +  _resultadosSolucion.get("c" + j));
+			}else {
+				System.out.print("  " +  _resultadosSolucion.get("c" + j));
+			}
+		}
+		System.out.println("");
+	}
 	
+	public void imprimirGrillaSolucion() {
+		for(int i = 0; i < _tamanio; i++) {
+			for(int j = 0; j < _tamanio; j++) {
+				if(_grillaSol[i][j] > 0) {
+					if(_grillaSol[i][j] > 9) {
+						System.out.print(" " +  _grillaSol[i][j]);
+					}else {
+						System.out.print("  " +  _grillaSol[i][j]);
+					}
+				}else {
+					System.out.print("  " + " ");
+				}
+			}
+			if(_resultadosSolucion.get("f" + i) > 9) {
+				System.out.println(" " +  _resultadosSolucion.get("f" + i));
+			}else {
+				System.out.println("  " +  _resultadosSolucion.get("f" + i));
+			}
+		}
+		
+		for(int j = 0; j < _tamanio ; j++) {
+			if(_resultadosSolucion.get("c" + j) > 9) {
+				System.out.print(" " +  _resultadosSolucion.get("c" + j));
+			}else {
+				System.out.print("  " +  _resultadosSolucion.get("c" + j));
+			}
+		}
+		System.out.println("");
+	}
+
+	/*
+	 * -------------- Metodos Privados ----------------
+	 */
+	
+	private int[][] generarGrilla(int tamanio) {
+		for(int i=0; i < _tamanio; i++) {
+			_resultados.put("f" + i, 0);
+			_resultados.put("c" + i, 0);
+		}
+		return new int[tamanio][tamanio];
+	}
+	private int[][] generarGrillaSolucion(int tamanio) {
+		int[][] grilla = new int[tamanio][tamanio];
+		
+		for(int i = 0; i < tamanio ; i++) {
+			_resultadosSolucion.put("f" + i, 0);
+			_resultadosSolucion.put("c" + i, 0);
+		}
+		
+		for(int i = 0; i < tamanio; i++) {
+			for(int j = 0; j < tamanio; j++) {
+				// Genero la grilla random
+				grilla[i][j] = rand(1,tamanio*2);
+				
+				// Sumando a cada fila
+				_resultadosSolucion.put("f" + i, _resultadosSolucion.get("f"+i) + grilla[i][j]);
+				
+				// Sumando a cada columna
+				_resultadosSolucion.put("c" + i, _resultadosSolucion.get("c"+i) + grilla[i][j]);
+				
+			}
+		
+		}
+		return grilla;
+	}
+	
+	private static int rand(int min, int max) {
+		Random r = new Random(); return r.nextInt(max-min+1) + min;
+	}
+	
+	private int obtenerResultado(String fila_o_col, int i) {
+		if(fila_o_col.equals("f") || fila_o_col.equals("c")) {
+			if(i >= 0 && i < _tamanio) {
+				return _resultados.get(fila_o_col + i);
+			}else {
+				throw new IllegalArgumentException("El numero de fila o columan debe estar en el rango: " + i);
+			}
+		}else {
+			throw new IllegalArgumentException("Debe introducir f - fila o ' - columna: " + fila_o_col);
+		}
+	}
 }
--- Ezequiel/JuegoCuadroMagico/src/logica/Juego.java
+++ Ezequiel/JuegoCuadroMagico/src/logica/Juego.java
@@ -1,24 +1,69 @@
 package logica;
 
+import java.util.Scanner;
+
 public class Juego {
 
-	public Juego() {
-		
-
+	GrillaJuego _grilla;
+	Boolean _estaJugando;
+	
+	public Juego(int tamanioDeGrilla) {
+		_grilla = new GrillaJuego(tamanioDeGrilla);
+		_estaJugando = false;
 	}
 	
-	public void iniciar() {}
-	
-	public void modificar() {}
-	
-	public boolean gano() { return false;}
-	
-	public boolean verificacionDeDatos() {return false;}
-	
-	
-	public static void main(String[] args) {
+	public void iniciar() {
+		_estaJugando = true;
+		Scanner ingreso = new Scanner(System.in);
+		String tmp;
+		int fila;
+		int columna;
+		int num;
 		
-		
-	}
+		while(_estaJugando) {
+			
+			
+			_grilla.imprimir();
+			
+//			Estaba viendo como se generaban los numeros
+//			_grilla.imprimirGrillaSolucion();
+			
+			System.out.println("Ingrese la fila y columna a modificar: 12 = (1,2) , 01 = (0,1) , etc" );
+			tmp = ingreso.nextLine();
+			fila = Integer.valueOf(tmp.toLowerCase().charAt(0)+"");	
+			columna = Integer.valueOf(tmp.toLowerCase().charAt(1)+"");
+			System.out.println("Ingrese el numero: ");
+			num = Integer.valueOf(ingreso.nextLine());
 
+			modificar(fila, columna, num);
+			
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			_grilla.imprimir();
+			
+//          Estaba viendo como se generaban los numeros 
+//			_grilla.imprimirGrillaSolucion();
+
+			
+			System.out.println("Coloque 1 si desea dejar de jugar: ");
+			_estaJugando = ingreso.nextLine() == "";
+		}
+	}
+	
+	public void modificar(int i, int j, int num) {
+		_grilla.modificar(i, j, num);
+	}
+	
+	public boolean gano() { 
+		return _grilla.estaCompletaMatriz();
+	}
+	
+	public boolean verificacionDeDatos() {
+		return false;
+	}
+	
 }
