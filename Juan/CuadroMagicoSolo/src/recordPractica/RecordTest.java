5d1e2b2e35abdb1e86b18907c1a04439969ba221
Juanos <68672009+Juano-C@users.noreply.github.com>
2023-04-02 14:45:01 -0300

    añado paquete record (#9)

---
+++ Juan/CuadroMagicoSolo/src/recordPractica/RecordTest.java
@@ -0,0 +1,33 @@
+package recordPractica;
+
+import java.util.TreeMap;
+import org.junit.Test;
+
+public class RecordTest
+{
+    TreeMap<Integer, String> tmap = new TreeMap<Integer, String>();
+
+    //si el map supera 10 posiciones
+    @Test
+    public void superaLargo(){}
+
+    //si hay key se excede
+    @Test
+    public void superaTiempo()
+    {
+        RecordPrueba.añadirElementos(tmap, 1, "Data0");
+    }
+
+    //nombre vacio (invalido)
+    @Test
+    public void nombreInvalido(){}
+
+    //elimina elemento
+    @Test
+    public void eliminarElemento(){}
+
+    //agregar elemento
+    @Test
+    public void agregarElemento(){}
+
+}
