7accedfe3c4593341826c7e9baa45e0ea765d69a
Esteban Lucero <este.lucero2212@gmail.com>
2023-04-04 21:05:11 -0300

    Agrego el mostrar puesto

--- TPMagicSquare/src/record/DatosRecord.java
+++ TPMagicSquare/src/record/DatosRecord.java
@@ -3,6 +3,8 @@ package record;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+
+
 /*
  * Creo que hay que poner metodos en privado
  */
@@ -11,13 +13,13 @@ public class DatosRecord
 
     //Si la key (tiempo) se ha excedido     |  Excepcion
     //Si no pone ningun nombre              |
-    public static void agregarElemento(TreeMap<Integer, String> recordMap, int key, String value)
+    public static void agregarElemento(TreeMap<Integer, String> recordMap, int tiempo, String nombre)
     {
         //parametros invalidos = excepcion
-        invalidParameters(key, value);
+        invalidParameters(tiempo, nombre);
 
         //agrega tiempo y nombre al map de records 
-        recordMap.put(key, value);
+        recordMap.put(tiempo, nombre);
 
         //quita ultimo elemento asi solo almacena 10 records
         removerUltimo(recordMap);
@@ -43,6 +45,8 @@ public class DatosRecord
     {
         Set<Map.Entry<Integer, String> > entrySet = recordMap.entrySet();
         Map.Entry<Integer, String>[] entryArray = entrySet.toArray(new Map.Entry[entrySet.size()]);
+        
+        
 
         for (int indiceMap = 0; indiceMap < recordMap.size(); indiceMap++)
         {
@@ -51,5 +55,49 @@ public class DatosRecord
             System.out.println();
         }
     }
+    
+     static String mostrarPuesto(TreeMap<Integer, String> recordMap,int puesto)
+    {
+        Set<Map.Entry<Integer, String> > entrySet = recordMap.entrySet();
+        Map.Entry<Integer, String>[] entryArray = entrySet.toArray(new Map.Entry[entrySet.size()]);
+        
+        
+        String salida = null;
+        
+        for (int indiceMap = 0; indiceMap < recordMap.size(); indiceMap++)
+        {
+        	
+        	if(puesto==indiceMap) {
+        		salida=  entryArray[indiceMap].getValue();
+        	}
+            
+        }
+        return salida;
+        
+    }
+    public static void mostrarKeys(TreeMap<Integer, String> recordMap) {
+    	for(Integer key:recordMap.keySet()) {
+    		System.out.println(key);
+    		System.out.println(recordMap.get(key));
+    	}
+    }
 
+
+
+public static void main(String[] args){
+	TreeMap<Integer, String> tmap = new TreeMap<Integer, String>();
+
+    DatosRecord.agregarElemento(tmap, 1000, "Data0");
+    DatosRecord.agregarElemento(tmap, 222, "Data1");
+    DatosRecord.agregarElemento(tmap, 30000, "Data2");
+    DatosRecord.agregarElemento(tmap, 41, "Data3");
+    DatosRecord.agregarElemento(tmap, 54, "Data4");
+    DatosRecord.agregarElemento(tmap, 6, "Data5");
+    DatosRecord.agregarElemento(tmap, 17, "Data6");
+    DatosRecord.agregarElemento(tmap, 86, "Data7");
+    DatosRecord.agregarElemento(tmap, 2, "Data8");
+    DatosRecord.agregarElemento(tmap, 10, "Data9");
+  
+    DatosRecord.mostrarKeys(tmap);
+}
 }
