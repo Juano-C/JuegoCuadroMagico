f1f1a7ec1f1554d560b54fee890f41300bac906e
David <daviddanielc25@gmail.com>
2023-04-08 09:42:39 -0300

    Mas soniditos al juego.

--- Ezequiel/TPMagicSquareV1.0/src/sonido/Audios.java
+++ Ezequiel/TPMagicSquareV1.0/src/sonido/Audios.java
@@ -7,13 +7,19 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 
+/**
+ * Clase encargada de contener las direcciones de audios asi como reproducirlos
+ * @author 
+ */
 public class Audios {
 
 	private static String _dir = System.getProperty("user.dir");
 	private static String _rutaMusica = _dir + "//src//sonido//musicGameTP.wav";
 	private static String _rutaSonidoClick = _dir + "//src//sonido//sonidoClick.wav";
+	private static String _rutaSonidoGano = _dir + "//src//sonido//sonidoWin.wav";
+	private static String _rutaSonidoPerdio = _dir + "//src//sonido//sonidoPerdio.wav";
+	private static String _rutaSonidoClickCelda = _dir + "//src//sonido//sonidoClickCelda.wav";
 	private static Clip _musica;
-	private static Clip _sonidoClick;
 	
 	public static void musicaPlay()  {
 		try {
@@ -37,25 +43,54 @@ public class Audios {
 	}
 	
 	public static void sonidoClick() {
+		reproducir(_rutaSonidoClick, (float) 0.2, false);
+	}
+	
+	public static void sonidoClickCelda() {
+		reproducir(_rutaSonidoClickCelda, (float) 0.2, false);
+	}
+
+	public static void sonidoPerdio() {
+		reproducir(_rutaSonidoPerdio, (float) 0.3, false);
+	}
+	
+	public static void sonidoGano() {
+		reproducir(_rutaSonidoGano, (float) 1.0, false);
+	}
+	
+	/**
+	 * Reproduce un archivo .wav pasadole su ruta. 
+	 * Volumen es un valor entre 1.0 y 0.0 donde 1.0 = 100% y 0.0 = 0% de volumen,
+	 * Recibe un tercer parametro para reproducirlo en loop de asi quererlo:
+	 * 			 false = no hay loop
+	 * 			 true  = hay loop
+	 * @param ruta
+	 * @param volumen
+	 * @param loop
+	 */
+	private static void reproducir(String ruta, Float volumen, boolean loop) {
 		try {
 			// Creo el archivo
-			File archivo = new File(_rutaSonidoClick);
+			File archivo = new File(ruta);
 			// Creo el Stream de Audio
 			AudioInputStream stream = AudioSystem.getAudioInputStream(archivo.getAbsoluteFile());
 			// Le asigno un clip a mi variable Clip
-			_sonidoClick = AudioSystem.getClip();
+			Clip sonido = AudioSystem.getClip();
 			// Abro el audio que esta en la en mi stream 
-			_sonidoClick.open(stream);
+			sonido.open(stream);
 			
 			// Volumen del clip
-			float volume = (float) 0.2; 
-			FloatControl gainControl = (FloatControl) _sonidoClick.getControl(FloatControl.Type.MASTER_GAIN);        
-			gainControl.setValue(20f * (float) Math.log10(volume));
-		
+			FloatControl gainControl = (FloatControl) sonido.getControl(FloatControl.Type.MASTER_GAIN);        
+			gainControl.setValue(20f * (float) Math.log10(volumen));
+			
+			// Si quiere que se reproduzca en loop
+			if(loop) {
+				sonido.loop(Clip.LOOP_CONTINUOUSLY);
+			}
+			
 			// Reproduzco el audio
-			_sonidoClick.start();
+			sonido.start();
 		} catch (Exception e) {
 		}
 	}
-	
 }
